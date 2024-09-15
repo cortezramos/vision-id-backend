2d8e4cd90b7ceb1137df4e58e718eeb21eb3d15d
cortezramos <cortezramos@hotmail.com>
2024-09-15 06:57:27 -0600

    first commit

---
+++ src/main/java/com/solinagro/visionid/VisionidApplication.java
@@ -0,0 +1,13 @@
+package com.solinagro.visionid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solinagro/visionid/VisionidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solinagro.visionid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
